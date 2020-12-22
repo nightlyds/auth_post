f02860a3f9234932beea8e8a83f55a0f9e297788
nightlyds <safonov.danil.cw@gmail.com>
2020-12-22 10:26:15 +0200

    Creating basic structure

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "regenerator-runtime/runtime";
+// import { Provider } from "react-redux";
+// import store from "./store/store.js";
+// import { Route, Switch, BrowserRouter } from "react-router-dom";
+import "normalize.css";
+import "animate.css";
+import "./basic/css/fonts_and_colors.css";
+
+function App() {
+  return (
+    <div className="App">
+        {/* <Provider store={store}>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={} />
+              <Route component={Error404} />
+            </Switch>
+          </BrowserRouter>
+        </Provider> */}
+    </div>
+  );
+}
+
+export default App;
